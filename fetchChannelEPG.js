24ee4e845590d1a7dad0a1bccbf960121cec5ee4
Ajay Shah <42646728+ajaykrshah@users.noreply.github.com>
2023-12-11 15:14:26 +0530

    Update fetchChannelEPG.js

--- fetchChannelEPG.js
+++ fetchChannelEPG.js
@@ -3,7 +3,7 @@
 const axios = require('axios');
 const fs = require('fs');
 
-const apiUrl = 'https://jiotv.data.cdn.jio.com/apis/v1.3/getepg/get?channel_id=143&offset=0';
+const apiUrl = 'http://jsonplaceholder.typicode.com/todos/1';
 
 axios.get(apiUrl)
   .then(response => {
