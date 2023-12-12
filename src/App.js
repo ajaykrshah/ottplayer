c21de0fa9d08242572bf4afbc62b6353bb403d52
Ajay Shah <42646728+ajaykrshah@users.noreply.github.com>
2023-12-12 09:40:53 +0530

    feature 1.2
    Added the EPG list also some major UI changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import fetchM3UPlaylist from './utils/m3uFetcher';
 import StreamList from './StreamList';
 import StreamFilter from './StreamFilter';
 import Player from './Player';
-
+import { extractChannelName, fetchEPGForChannel } from './utils/playlistGenerator';
+import ReactPlayer from 'react-player';
 const App = () => {
   const [streams, setStreams] = useState([]);
   const [selectedStream, setSelectedStream] = useState(null);
@@ -35,11 +36,6 @@ const App = () => {
     setSelectedStream(streams[index]);
   };
 
-  const handleClosePlayer = () => {
-    setSelectedStream(null);
-  };
-
-
   return (
     <div className={isDarkMode ? 'dark-mode' : 'light-mode'}>
       <div className="app-container">
@@ -47,11 +43,54 @@ const App = () => {
         <StreamFilter streams={streams} setFilteredStreams={setFilteredStreams} />
         <ThemeToggle isDarkMode={isDarkMode} toggleTheme={toggleTheme} />
         {selectedStream ? (
-          <Player selectedStream={selectedStream} handleClosePlayer={handleClosePlayer} />
+          <Player selectedStream={selectedStream} goBack={() => setSelectedStream(null)}/>
         ) : (
           <StreamList streams={filteredStreams} handleStreamClick={handleStreamClick} />
         )}
 
+      </div>
+    </div>
+  );
+};
+
+const App2 = () => {
+  const [channelListData, setChannelListData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+          
+        const channelInfo = {
+            "channel_id": "144",
+            "channel_name": "Colors HD",
+            "logoUrl": "http://jiotv.catchup.cdn.jio.com/dare_images/images/Colors_HD.png"
+          };
+          
+      
+        const data = await fetchEPGForChannel(channelInfo);
+        setChannelListData(data);
+      } catch (error) {
+        // Handle errors if necessary
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Your React App</h1>
+      {/* <ReactPlayer url='http://192.168.101.30:3500/getm3u8/173/master.m3u8' controls={true} playing={true}/> */}
+
+      <div className="player-wrapper">
+        <ReactPlayer
+          className="react-player"
+          url='http://192.168.101.30:3500/getm3u8/173/master.m3u8'
+          width="100%"
+          height="100%"
+          controls // Add other props like controls, playing, etc. as needed
+        />
       </div>
     </div>
   );
